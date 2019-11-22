a8118ef5c972765c78e5d657daa264a795694c2e
renatoppuccini <renatoppuccini@gmail.com>
2019-11-22 09:29:56 -0500

    deleting file

--- dynatrace-java-env.sh
+++
@@ -1,6 +0,0 @@
-# 32 bit Java: dynatrace-java-env.sh
-# 64 bit Java: dynatrace-java-env.sh 64
-DT_DIR=`dirname -- "$0"`
-. $DT_DIR/dynatrace-env.sh
-export JAVA_OPTS="${JAVA_OPTS} -agentpath:$DT_DIR/agent/lib$1/liboneagentloader.so"
-export JAVA_OPTIONS="${JAVA_OPTIONS} -agentpath:$DT_DIR/agent/lib$1/liboneagentloader.so"
